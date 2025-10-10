d7f84171a6b51e18cb460b23f477899ae7281e0d
the00creeper <clarkaldreipedreramercado@gmail.com>
2025-10-10 11:21:48 +0800

    Quartz sync: Oct 10, 2025, 11:21 AM

--- quartz/components/Graph.tsx
+++ quartz/components/Graph.tsx
@@ -51,7 +51,7 @@ const defaultOptions: GraphOptions = {
     centerForce: 0.31,
     linkDistance: 57,
     fontSize: 0.6,
-    opacityScale: 4,
+    opacityScale: 6,
     showTags: true,
     removeTags: [],
     focusOnHover: true,
